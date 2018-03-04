2dbbeb300c72499d5c82173ab9c7aa17ad09feec
Saketh Katari <katari15045@iiitd.ac.in>
2018-03-05 02:31:01 +0530

    src : Adds login -
    -> No backend
    -> Need to include bottomNavBar in login screen

--- code/src/IIITDLibrary/app/src/main/java/com/github/katari15045/iiitdlibrary/LoginFragment.java
+++ code/src/IIITDLibrary/app/src/main/java/com/github/katari15045/iiitdlibrary/LoginFragment.java
@@ -1,17 +1,64 @@
 package com.github.katari15045.iiitdlibrary;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginFragment extends Fragment {
+
+    private View view = null;
+    private AppCompatActivity activity = null;
+    private EditText editTextUsername = null;
+    private EditText editTextPassword = null;
+    private Button buttonLogin = null;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        this.view = inflater.inflate(R.layout.fragment_login, container, false);
+        this.activity = (AppCompatActivity)view.getContext();
+        changeActionBarTitle();
+        collectViews();
+        return view;
+    }
+
+    private void changeActionBarTitle(){
+        ActionBar actionBar = activity.getSupportActionBar();
+        actionBar.setTitle(view.getResources().getString(R.string.login_fragment_title));
+    }
+
+    private void collectViews(){
+        editTextUsername = view.findViewById(R.id.fragment_login_edit_text_username);
+        editTextPassword = view.findViewById(R.id.fragment_login_edit_text_password);
+        buttonLogin = view.findViewById(R.id.fragment_login_button_login);
+        buttonLogin.setOnClickListener(new LoginListener(view.getContext()));
+    }
+}
+
+class LoginListener implements View.OnClickListener{
+
+    private Context context = null;
+
+    LoginListener(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(context, "Login", Toast.LENGTH_SHORT).show();
+        BottomNavigationView bottomNavigationView = MainActivity.getBottomNavBar();
+        bottomNavigationView.getMenu().getItem(2).setTitle(context.getResources().
+                getString(R.string.bottom_nav_bar_title_profile));
     }
 }
--- code/src/IIITDLibrary/app/src/main/java/com/github/katari15045/iiitdlibrary/MainActivity.java
+++ code/src/IIITDLibrary/app/src/main/java/com/github/katari15045/iiitdlibrary/MainActivity.java
@@ -12,8 +12,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    static Fragment currentFragment = null;
     private NavDrawer navDrawer = null;
-    private BottomNavigationView bottomNavigationBar = null;
+    private static BottomNavigationView bottomNavigationBar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         navDrawer = new NavDrawer(this);
         addBottomNavBar();
-        displayHome();
+        if(currentFragment == null){
+            displayHome();
+        }else{
+            replaceFragment(currentFragment);
+        }
     }
 
     private void addBottomNavBar(){
@@ -48,14 +53,20 @@ public class MainActivity extends AppCompatActivity {
         transaction.replace(R.id.activity_main_fragment_container, fragment);
         transaction.commit();
     }
+
+    static BottomNavigationView getBottomNavBar(){
+        return bottomNavigationBar;
+    }
 }
 
 class BottomNavBarListener implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     private Context context = null;
+    private AppCompatActivity activity = null;
 
     BottomNavBarListener(Context context){
         this.context = context;
+        this.activity = (AppCompatActivity)context;
     }
 
     @Override
@@ -65,8 +76,19 @@ class BottomNavBarListener implements BottomNavigationView.OnNavigationItemSelec
         }else if(item.getItemId() == R.id.menu_bottom_nav_bar_search){
             Toast.makeText(context, "Search", Toast.LENGTH_SHORT).show();
         }else if(item.getItemId() == R.id.menu_bottom_nav_bar_login){
-            Toast.makeText(context, "Login", Toast.LENGTH_SHORT).show();
+            if(item.getTitle().equals(context.getResources().getString(
+                    R.string.bottom_nav_bar_title_login))){
+                LoginFragment loginFragment = new LoginFragment();
+                replaceFragment(loginFragment);
+                MainActivity.currentFragment = loginFragment;
+            }
         }
         return false;
+    }
+
+    void replaceFragment(Fragment fragment){
+        FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.activity_main_fragment_container, fragment);
+        transaction.commit();
     }
 }
